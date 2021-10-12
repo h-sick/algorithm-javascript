5046f81d6e491c15136db537675828987388029a
h-sick <luna23825@gmail.com>
2021-10-12 11:43:27 +0900

    refac: remove groupInfo map
    결과에 영향 끼치지 않는 변수 삭제

--- baekjoon/silver/16165.js
+++ baekjoon/silver/16165.js
@@ -3,13 +3,11 @@ const inputs = require('fs').readFileSync('16165.txt').toString().split('\n');
 let n = inputs[0].split('')[0];
 let m = inputs[0].split('')[2];
 
-let groupInfo = new Map();
 let member = new Map();
 
 let index = 1;
 for (let i = 0; i < n; i++) {
   const memberNumber = Number(inputs[index + 1]);
-  groupInfo.set(inputs[index], inputs[index + 1]);
 
   for (let j = 0; j < memberNumber; j++) {
     member.set(inputs[index + 2 + j], inputs[index]);
