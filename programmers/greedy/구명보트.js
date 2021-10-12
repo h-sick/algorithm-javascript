dd6e8be3885a6008fe91e3e5c2ae2e386a9833b7
h-sick <luna23825@gmail.com>
2021-10-12 23:47:41 +0900

    feat: solved
    투포인터 활용하여 시간복잡도 해결

--- programmers/greedy/구명보트.js
+++ programmers/greedy/구명보트.js
@@ -20,29 +20,21 @@
 //   return saved;
 // }
 
-function solution(people, limit) {
-  people.sort((a, b) => b - a);
-
-  let stack = [];
-  let saved = 0;
-  while (people.length) {
-    if (people[0] + 40 <= limit) {
-      let index = 1;
-      while (index < people.length) {
-        if (people[0] + people[index] <= limit) {
-          people.splice(index, 1);
-          break;
-        }
-        index++;
-      }
-    }
-    stack.push(people.shift());
-    saved++;
-  }
-  return saved;
-}
-
 console.log(solution([70, 50, 80, 50], 100)); // 3
 console.log(solution([70, 80, 50], 100)); // 3
 console.log(solution([40, 50, 150, 160], 200)); // 2
 console.log(solution([100, 500, 500, 900, 950], 1000)); // 3
+
+function solution(people, limit) {
+  people.sort((a, b) => b - a);
+
+  let left = 0,
+    right = people.length - 1,
+    count = 0;
+  while (left <= right) {
+    if (people[left] + people[right] <= limit) right--;
+    left++;
+    count++;
+  }
+  return count;
+}
