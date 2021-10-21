4fe78a135fdbe1c98f5e37b3375bc491db7b0569
h-sick <luna23825@gmail.com>
2021-10-22 00:49:04 +0900

    feat: solved 2655.js
    가장 높은 탑
    다이나믹 프로그래밍

--- baekjoon/gold/js/2655.js
+++ baekjoon/gold/js/2655.js
@@ -6,32 +6,59 @@ const inputs = require('fs')
   .split('\n');
 let n = inputs.shift();
 let bricks = inputs.map((brick, i) => [...brick.split(' ').map(Number), i]);
-
 bricks.sort((a, b) => b[0] - a[0]);
 
-// 몇번째 벽돌로 쌓을 수 있는지, 지금까지 탑 높이 합산, 벽돌의 인덱스
-let dp = Array.from({ length: n }, () => [0, 0, null]);
-dp[0] = [1, bricks[0][1], null];
+let dp = Array(n).fill(0);
+dp[0] = bricks[0][1];
 
 for (let i = 1; i < n; i++) {
-  let max = [0, 0, null];
+  let maxHeight = 0;
   for (let j = i - 1; j >= 0; j--) {
-    if (bricks[j][2] > bricks[i][2]) {
-      if (dp[j][0] > max[0] || (dp[j][0] === max[0] && dp[j][1] > max[1])) {
-        max = [dp[j][0], dp[j][1], bricks[i][3] + 1];
-        if (j === 0) dp[0][2] = 1;
-      }
+    if (bricks[j][2] > bricks[i][2] && dp[j] > maxHeight) {
+      maxHeight = dp[j];
     }
-    dp[i][0] = max[0] + 1;
   }
-  dp[i][1] += max[1] + bricks[i][1];
-  dp[i][2] = max[2] || null;
+  dp[i] = maxHeight + bricks[i][1];
 }
 
-dp.sort((a, b) => b[2] - a[2]).forEach((brick, i) => {
-  if (i === 0) console.log(brick[0]);
-  brick[2] && console.log(brick[2]);
-});
+let answer = [];
+let max = Math.max(...dp);
+for (let i = dp.length - 1; i >= 0; i--) {
+  if (max === dp[i]) {
+    answer.push(bricks[i][3] + 1);
+    max -= bricks[i][1];
+  }
+}
+console.log(answer.length);
+answer.forEach((brick) => console.log(brick));
+
+// let bricks = inputs.map((brick, i) => [...brick.split(' ').map(Number), i]);
+
+// bricks.sort((a, b) => b[0] - a[0]);
+
+// // 몇번째 벽돌로 쌓을 수 있는지, 지금까지 탑 높이 합산, 벽돌의 인덱스
+// let dp = Array.from({ length: n }, () => [0, 0, null]);
+// dp[0] = [1, bricks[0][1], null];
+
+// for (let i = 1; i < n; i++) {
+//   let max = [0, 0, null];
+//   for (let j = i - 1; j >= 0; j--) {
+//     if (bricks[j][2] > bricks[i][2]) {
+//       if (dp[j][0] > max[0] || (dp[j][0] === max[0] && dp[j][1] > max[1])) {
+//         max = [dp[j][0], dp[j][1], bricks[i][3] + 1];
+//         if (j === 0) dp[0][2] = 1;
+//       }
+//     }
+//     dp[i][0] = max[0] + 1;
+//   }
+//   dp[i][1] += max[1] + bricks[i][1];
+//   dp[i][2] = max[2] || null;
+// }
+
+// dp.sort((a, b) => b[2] - a[2]).forEach((brick, i) => {
+//   if (i === 0) console.log(brick[0]);
+//   brick[2] && console.log(brick[2]);
+// });
 
 // // 넓이, 높이, 무게
 // // bricks.sort((a, b) => b[0] - a[0]);
