5757eea8cdb2e1ce83a019481389f0d7331feba2
h-sick <luna23825@gmail.com>
2021-11-08 20:52:01 +0900

    feat: solved 735.js
    stack

---
+++ leetcode/735.js
@@ -0,0 +1,32 @@
+// Asteroid Collision
+
+function solution(asteroids) {
+  let stack = [],
+    addNegative;
+  for (let i = 0; i < asteroids.length; i++) {
+    if (stack[stack.length - 1] > 0 && asteroids[i] < 0) {
+      while (stack[stack.length - 1] > 0) {
+        addNegative = null;
+        if (Math.abs(stack[stack.length - 1]) === Math.abs(asteroids[i])) {
+          stack.pop();
+          break;
+        }
+        if (Math.abs(stack[stack.length - 1]) > Math.abs(asteroids[i])) break;
+        if (Math.abs(stack[stack.length - 1]) < Math.abs(asteroids[i])) {
+          stack.pop();
+          addNegative = asteroids[i];
+        }
+      }
+      if (addNegative) stack.push(addNegative);
+    } else stack.push(asteroids[i]);
+  }
+  return stack;
+}
+
+console.log(solution([5, 10, -5])); // [5,10]
+console.log(solution([8, -8])); // []
+console.log(solution([10, 2, -5])); // [10]
+console.log(solution([-2, -1, 1, 2])); // [-2,-1,1,2]
+console.log(solution([-2, -2, 1, -1])); // [-2, -2]
+console.log(solution([-2, -2, 1, -2])); // [-2, -2, -2];
+console.log(solution([10, 2, -5])); // [10, -5];
