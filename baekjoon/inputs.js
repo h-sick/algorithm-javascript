98c894af1622edc01c594b3b079005c34cd1b8fe
h-sick <luna23825@gmail.com>
2022-01-28 15:02:01 +0900

    feat: add baekjoon/inputs.js to get input value by one function

---
+++ baekjoon/inputs.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const inputs = require('fs')
+  .readFileSync(
+    process.platform === 'linux'
+      ? 'dev/stdin'
+      : path.join(
+          __dirname,
+          `../text/${path.basename(__filename).split('.')[0]}.txt`
+        )
+  )
+  .toString()
+  .split('\n');
