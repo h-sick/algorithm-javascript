1c7ab21cf78d391d88ee21348f13ab9192497d39
h-sick <luna23825@gmail.com>
2022-03-03 16:59:01 +0900

    feat: re solved 2644.js
    - 촌수계산
    - 그래프 이론
    - 그래프 탐색
    - 너비 우선 탐색
    - 깊이 우선 탐색

--- baekjoon/silver/js/2644.js
+++ baekjoon/silver/js/2644.js
@@ -1,25 +1,35 @@
 // 촌수계산
 
+const path = require('path');
 const inputs = require('fs')
-  .readFileSync(process.platform === 'linux' ? 'dev/stdin' : '../text/2644.txt')
+  .readFileSync(
+    process.platform === 'linux'
+      ? 'dev/stdin'
+      : path.join(
+          __dirname,
+          `../text/${path.basename(__filename).split('.')[0]}.txt`
+        )
+  )
   .toString()
+  .trim()
   .split('\n');
 
-let [n, m] = [+inputs[0], +inputs[2]];
-let [a, b] = inputs[1].split(' ').map(Number);
-let relatives = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+const [n, m] = [+inputs[0].trim(), +inputs[2].trim()];
+const [a, b] = inputs[1].split(' ').map(Number);
+
+const relatives = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
 for (let i = 3; i < m + 3; i++) {
   const [parent, child] = inputs[i].split(' ').map(Number);
   relatives[child][parent] = 1;
   relatives[parent][child] = 1;
 }
 
-let checked = Array(n + 1).fill(0);
+const checked = Array(n + 1).fill(0);
 checked[a] = 1;
 
-let found = false,
-  answer;
-function dfs(start, count) {
+let found = false;
+let answer = -1;
+const dfs = (start, count) => {
   if (found) return;
   for (let i = 1; i <= 100; i++) {
     if (relatives[start][i] === 1 && checked[i] === 0) {
@@ -32,7 +42,44 @@ function dfs(start, count) {
       dfs(i, count + 1);
     }
   }
-  if (!answer) answer = -1;
-}
+};
 dfs(a, 0);
+
 console.log(answer);
+
+// const inputs = require('fs')
+//   .readFileSync(process.platform === 'linux' ? 'dev/stdin' : '../text/2644.txt')
+//   .toString()
+//   .split('\n');
+
+// let [n, m] = [+inputs[0], +inputs[2]];
+// let [a, b] = inputs[1].split(' ').map(Number);
+// let relatives = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+// for (let i = 3; i < m + 3; i++) {
+//   const [parent, child] = inputs[i].split(' ').map(Number);
+//   relatives[child][parent] = 1;
+//   relatives[parent][child] = 1;
+// }
+
+// let checked = Array(n + 1).fill(0);
+// checked[a] = 1;
+
+// let found = false,
+//   answer;
+// function dfs(start, count) {
+//   if (found) return;
+//   for (let i = 1; i <= 100; i++) {
+//     if (relatives[start][i] === 1 && checked[i] === 0) {
+//       checked[i] = 1;
+//       if (i === b) {
+//         found = true;
+//         answer = count + 1;
+//         return;
+//       }
+//       dfs(i, count + 1);
+//     }
+//   }
+//   if (!answer) answer = -1;
+// }
+// dfs(a, 0);
+// console.log(answer);
