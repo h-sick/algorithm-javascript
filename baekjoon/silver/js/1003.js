80e3302ad420b9828447be171c2f07bce839bb98
h-sick <luna23825@gmail.com>
2022-07-23 16:43:48 +0900

    feat: add another solve baekjoon/silver/1003
    - 피보나치 함수
    - 다이나믹 프로그래밍

--- baekjoon/silver/js/1003.js
+++ baekjoon/silver/js/1003.js
@@ -17,14 +17,33 @@ const inputs = require('fs')
 const n = +inputs[0];
 const nums = inputs.slice(1, n + 1).map(Number);
 
-nums.forEach(num => {
-  let zero = 1;
-  let one = 0;
+const memo = Array.from(Array(41), () => Array(2).fill(0));
 
-  for (let i = 1; i <= num; i++) {
-    const temp = zero;
-    zero = one;
-    one = temp + one;
+const fibonacci = num => {
+  if (num === 0) return (memo[num] = [1, 0]);
+  if (num === 1) return (memo[num] = [0, 1]);
+
+  if (memo[num][0] !== 0 && memo[num][1] !== 0) {
+    return memo[num];
   }
-  console.log(zero + ' ' + one);
+
+  const [zero1, one1] = fibonacci(num - 1);
+  const [zero2, one2] = fibonacci(num - 2);
+  return (memo[num] = [zero1 + zero2, one1 + one2]);
+};
+
+nums.forEach(num => {
+  console.log(fibonacci(num).join(' '));
 });
+
+// nums.forEach(num => {
+//   let zero = 1;
+//   let one = 0;
+
+//   for (let i = 1; i <= num; i++) {
+//     const temp = zero;
+//     zero = one;
+//     one = temp + one;
+//   }
+//   console.log(zero + ' ' + one);
+// });
