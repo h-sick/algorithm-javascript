de7d925a2c37c8fe6a07b67ad7a60b2184e66eab
h-sick <luna23825@gmail.com>
2022-08-07 22:15:33 +0900

    feat: solved 2xn_타일링.js
    - 피보나치 수열
    - dp

---
+++ programmers/level2/2xn_타일링.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  const dp = Array(n + 1).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007;
+  }
+  return dp[n];
+}
+
+console.log(solution(4)); // 5
