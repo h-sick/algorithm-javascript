897569ce31d7250f33285be97f44bdc6d90f306c
h-sick <luna23825@gmail.com>
2022-08-11 23:59:37 +0900

    refactor: remove Array.prototype.pop parameter

--- programmers/level2/큰_수_만들기.js
+++ programmers/level2/큰_수_만들기.js
@@ -2,7 +2,7 @@ function solution(number, k) {
   const answer = [];
   for (let num of number) {
     while (k > 0 && answer[answer.length - 1] < num) {
-      answer.pop(num);
+      answer.pop();
       k -= 1;
     }
     answer.push(num);
