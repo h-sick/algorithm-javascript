d866de90f51b49b46880da0f5f6bc847ff71ad98
h-sick <luna23825@gmail.com>
2022-09-20 17:40:11 +0900

    solved 283_Move_Zeroes
    - two pointers

---
+++ leetcode/283_Move_Zeroes.js
@@ -0,0 +1,11 @@
+var moveZeroes = function (nums) {
+  for (let i = 0, j = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      j += 1;
+    }
+  }
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12])); // [1,3,12,0,0]
+console.log(moveZeroes([0])); // [0]
